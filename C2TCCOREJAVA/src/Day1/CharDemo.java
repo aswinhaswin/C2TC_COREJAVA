a4087b95a74b82aa7e31401f3ea19e2ecaf15fdd
haswinhaswin014@gmail.com <aswin@192.168.1.33>
2025-07-21 18:45:26 +0530

    Day 1

---
+++ C2TCCOREJAVA/src/Day1/CharDemo.java
@@ -0,0 +1,22 @@
+package Day1;
+
+public class CharDemo {
+
+	public static void main(String[] args) {
+		char a='a';
+		System.out.println(a);
+		char ch1=65;
+		System.out.println(ch1);
+		char var1 = '\u00A7';
+		System.out.println(var1);
+		char var2 = '\u20AC';
+		System.out.println(var2);
+		int b = 'B';
+		System.out.println(b);
+		char vv='\u00c4';
+		System.out.println(vv);
+		
+
+	}
+
+}
---
+++ C2TCCOREJAVA/src/Day1/DataTypesDemo.java
@@ -0,0 +1,20 @@
+package Day1;
+
+public class DataTypesDemo {
+
+	public static void main(String[] args) {
+		int v1 = 9 / 2; 
+		float v2 = 101 / 61;
+		double v3 = 10d / 6d;
+		float v4 = 5/2.5f; 
+		System.out.println("value 1=" + v1); 
+		System.out.println("value 2=" + v2);
+		System.out.println("value 3=" + v3);
+		System.out.println("value 4=" + v4);
+		int marker = 512;
+		double percentage = marker * 0.46f;
+		System.out.println("percentage :" + percentage);
+		
+	}
+
+}

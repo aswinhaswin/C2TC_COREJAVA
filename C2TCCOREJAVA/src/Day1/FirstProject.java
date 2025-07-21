f08e7ca5e3b4c80ab62043d0a822b861eb0db9b5
haswinhaswin014@gmail.com <aswin@LAPTOP-ADOQ49B2>
2025-07-21 15:53:31 +0530

    addition of two numbers

--- C2TCCOREJAVA/src/Day1/FirstProject.java
+++ C2TCCOREJAVA/src/Day1/FirstProject.java
@@ -1,7 +1,5 @@
 package Day1;
-
-public class FirstProject {
-
+public class FirstProject {  
 	public static void main(String[] args) {//jvm start from main program
 		System.out.println("WELCOME TO JAVA CLASS");
 		

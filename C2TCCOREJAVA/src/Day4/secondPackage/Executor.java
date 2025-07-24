be8ca957544bf6b9c9d01a87b28c22f3f59ba591
haswinhaswin014@gmail.com <aswin@192.168.1.33>
2025-07-24 23:10:32 +0530

    Day 4

---
+++ C2TCCOREJAVA/src/Day4/secondPackage/Executor.java
@@ -0,0 +1,13 @@
+package Day4.secondPackage;
+import Day4.FirstPackage.Base;
+public class Executor {
+
+	public static void main(String[] args) {
+		
+				Base b1 = new Base();
+
+				b1.methodPublic();
+				b1.varPublic = 41;
+				b1.methodPublic();
+			}
+		}
